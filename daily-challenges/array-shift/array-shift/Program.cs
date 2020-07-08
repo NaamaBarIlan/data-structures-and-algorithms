4eb8b079a8ba6f304e457cc3e9b38fa394562481
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-07 18:44:28 -0700

    Set up files, added to Main and insertShiftarray method

---
+++ daily-challenges/array-shift/array-shift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace array_shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray;
+            int insertValue;
+            insertShiftArray(inputArray, insertValue);
+        }
+
+        static void insertShiftArray(int[] array, int value)
+        {
+            int[] newArray = new int[array.Length + 1];
+
+
+        }
+    }
+}
