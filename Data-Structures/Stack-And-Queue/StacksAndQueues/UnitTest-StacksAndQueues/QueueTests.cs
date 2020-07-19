7ce046ab4d23bcd21428156642d15a8ca644d9ee
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-19 09:29:31 -0700

    Added the Stacks and Queue classes and tests.

---
+++ Data-Structures/Stack-And-Queue/StacksAndQueues/UnitTest-StacksAndQueues/QueueTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues;
+using Xunit;
+
+namespace UnitTest_StacksAndQueues
+{
+    public class QueueTests
+    {
+
+        public void CanEnqueueIntoQueue()
+        {
+            // Arrange
+            Queue que = new Queue();
+
+            // Act
+            que.Enqueue("Josie Cat");
+            que.Enqueue("Belle Kitty");
+
+            // Assert
+            Assert.Equal("Josie Cat", que.Front.Value);
+        }
+    }
+}
