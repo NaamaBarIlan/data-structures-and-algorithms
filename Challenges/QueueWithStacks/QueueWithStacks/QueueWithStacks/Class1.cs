8ca66d0e012fa25350a482eb96220916f8f217f1
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-20 19:49:09 -0700

    Updated Queue with Stacks README.

---
+++ Challenges/QueueWithStacks/QueueWithStacks/QueueWithStacks/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QueueWithStacks
+{
+    public class Class1
+    {
+    }
+}
--- Data-Structures/Stack-And-Queue/StacksAndQueues/StacksAndQueues/Stack.cs
+++ Data-Structures/Stack-And-Queue/StacksAndQueues/StacksAndQueues/Stack.cs
@@ -15,5 +15,22 @@ namespace StacksAndQueues
             node.Next = Top;
             Top = node;
         }
+
+        public string Pop(string value)
+        {
+            // check isEmpty
+            if()
+            {
+                return null;
+            }
+            // create a temp node
+            Node node = new Node(value);
+            Node temp = new Node(value);
+            temp = Top;
+            Top = Top.Next;
+            Top.Next = null;
+            return temp.value;
+
+        }
     }
 }
