13cf72e978dd64f3a3144956750d13afdd272120
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-24 17:43:34 -0700

    CanReturnPreOrderTraversal() test passes.

--- Data-Structures/Trees/Trees/XUnitTest - Tree/UnitTest1.cs
+++ Data-Structures/Trees/Trees/XUnitTest - Tree/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Trees;
 using System.Xml;
+using System.Collections.Generic;
 
 namespace XUnitTest___Tree
 {
@@ -46,5 +47,43 @@ namespace XUnitTest___Tree
             Assert.Equal('c', tree.Root.RightChild.Value);
         }
 
+        [Fact]
+        public void CanReturnPreOrderTraversal()
+        {
+            // Arrange
+
+            Tree<char> tree = new Tree<char>();
+
+            Node<char> root = new Node<char>('a');
+            Node<char> b = new Node<char>('b');
+            Node<char> c = new Node<char>('c');
+            Node<char> d = new Node<char>('d');
+            Node<char> e = new Node<char>('e');
+            Node<char> f = new Node<char>('f');
+            Node<char> g = new Node<char>('g');
+
+            tree.Root = root;
+
+            root.LeftChild = b;
+            root.RightChild = c;
+
+            b.LeftChild = d;
+            b.RightChild = e;
+
+            c.LeftChild = f;
+            c.RightChild = g;
+
+            List<char> order = new List<char>()
+            {
+                'a', 'b', 'd', 'e', 'c', 'f', 'g'
+            };
+
+            // Act
+
+            List<char> preOrder = tree.PreOrder(tree.Root);
+
+            // Assert
+            Assert.Equal(order, preOrder);
+        }
     }
 }
