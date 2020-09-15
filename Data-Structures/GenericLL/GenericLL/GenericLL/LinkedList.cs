18378f8b17915b7958aed371784100dd78fedead
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-09-14 19:37:28 -0700

    Added a GenericLL project

---
+++ Data-Structures/GenericLL/GenericLL/GenericLL/LinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericLL
+{
+    public class LinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+        public Node<T> Current { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+    }
+}
