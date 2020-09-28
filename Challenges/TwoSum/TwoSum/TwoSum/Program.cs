3acd0f3d5393ad0618971b6cc6edaacdf0288e65
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-09-27 19:21:46 -0700

    Completed TwoSum method and sample I/O

--- Challenges/TwoSum/TwoSum/TwoSum/Program.cs
+++ Challenges/TwoSum/TwoSum/TwoSum/Program.cs
@@ -2,11 +2,43 @@
 
 namespace TwoSum
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Two Sum");
+
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] output = TwoSum(nums, target);
+
+            Console.WriteLine("[{0}]", string.Join(", ", output));
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int first;
+            int second;
+            int[] output = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                first = nums[i];
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    second = nums[j];
+
+                    if (first + second == target)
+                    {
+                        output[0] = i;
+                        output[1] = j;
+                        return output;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
