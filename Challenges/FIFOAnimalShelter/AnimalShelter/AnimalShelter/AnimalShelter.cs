7e5316cd68ca60fddcd78fd93f7f0a9bc133dbcc
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-10-05 20:01:17 -0700

    Added XUnit tests to FIFOAnimalShelter

--- Challenges/FIFOAnimalShelter/AnimalShelter/AnimalShelter/AnimalShelter.cs
+++ Challenges/FIFOAnimalShelter/AnimalShelter/AnimalShelter/AnimalShelter.cs
@@ -9,6 +9,20 @@ namespace AnimalShelter
 
         // enqueue(animal)  adds animal to the shelter. animal can be either a dog or a cat object.
 
+        public static Queue Enqueue(Animal animal)
+        {
+            Queue shelter = new Queue();
+
+            if (shelter.Front == null)
+            {
+                shelter.Front = animal;
+            }
+
+            shelter.Rear = animal;
+
+            return shelter;
+        }
+
 
         // dequeue(pref): returns either a dog or a cat. If pref is not "dog" or "cat" then return null.
     }
