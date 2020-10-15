cc99b202a36d978da5f12367ac2c365b768cc6d7
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-10-14 19:05:44 -0700

    Added README to Anagrams

--- Challenges/Anagrams/Anagrams/Anagrams/Program.cs
+++ Challenges/Anagrams/Anagrams/Anagrams/Program.cs
@@ -8,5 +8,17 @@ namespace Anagrams
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// Given two strings, determines if the second string is an 
+        /// anagram of the first string. 
+        /// </summary>
+        /// <param name="str1">The first string that is being analyzed</param>
+        /// <param name="str2">The second string that is being analyzed</param>
+        /// <returns>A true if the second string is an anagram of the first, or false</returns>
+        public static bool ValidAnagram(string str1, string str2)
+        {
+
+        }
     }
 }
