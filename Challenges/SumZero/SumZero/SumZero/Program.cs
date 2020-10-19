cbeb1635c03ae75516ea71429e5be342b4df1c99
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-10-19 13:08:14 -0700

    Created SumZero project.

---
+++ Challenges/SumZero/SumZero/SumZero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int SumZero(int[] arr)
+        {
+            int left = arr[0];
+            int right = arr[arr.Length - 1];
+
+            while(left < right)
+            {
+
+            }
+        }
+    }
+}
