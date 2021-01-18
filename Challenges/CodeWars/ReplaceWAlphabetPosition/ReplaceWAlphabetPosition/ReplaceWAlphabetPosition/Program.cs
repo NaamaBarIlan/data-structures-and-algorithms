640182f4470e1ad6135bc7149cad65b1faafcb53
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-01-17 18:47:03 -0800

    'Replace with Alphabet Position' project set up. Updated README

---
+++ Challenges/CodeWars/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReplaceWAlphabetPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
