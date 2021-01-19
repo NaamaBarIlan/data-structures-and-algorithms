5a434ef3b6761019eb96567f874068dbeeaeefb1
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-01-18 18:47:57 -0800

    Added AlphabetPosition method

--- Challenges/CodeWars/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/Program.cs
+++ Challenges/CodeWars/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/ReplaceWAlphabetPosition/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Text;
 
 namespace ReplaceWAlphabetPosition
 {
@@ -8,5 +10,34 @@ namespace ReplaceWAlphabetPosition
         {
             Console.WriteLine("Hello World!");
         }
-    }
+
+		public static string AlphabetPosition(string str)
+		{
+			Hashtable alphabetTable = new Hashtable();
+			string alphabetString = "abcdefghijklmnopqrstuvwxyz";
+			StringBuilder SB = new StringBuilder();
+			string lowerCaseString = str.ToLower();
+
+			for (int i = 1; i < alphabetString.Length + 1; i++)
+			{
+				alphabetTable.Add("alphabetString[i]", "i");
+			}
+
+			foreach (char item in lowerCaseString)
+			{
+				if (!char.IsLetter(item))
+				{
+					continue;
+				}
+				else
+				{
+					SB.Append(alphabetTable["item"]);
+				}
+
+			}
+
+			return SB.ToString();
+		}
+
+	}
 }
