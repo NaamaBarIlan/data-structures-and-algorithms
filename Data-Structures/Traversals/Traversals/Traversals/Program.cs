b0635a054a2f627fe90c1a651f5a11778078774d
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-01-26 19:21:03 -0800

    Added CountLLNodes to Traversals

--- Data-Structures/Traversals/Traversals/Traversals/Program.cs
+++ Data-Structures/Traversals/Traversals/Traversals/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LinkedListLibrary;
+using LinkedListTests;
+using System;
+using System.Collections.Generic;
 
 namespace Traversals
 {
@@ -7,7 +10,29 @@ namespace Traversals
         static void Main(string[] args)
         {
             Console.WriteLine("Traversals");
+
         }
+
+        //LinkedLists
+
+        //1. Counting the amount of nodes in Linked List
+
+        public static int CountLLNodes(LinkedList linkedList)
+        {
+            int counter = 0;
+            Node current = linkedList.Head;
+
+            while (current != null)
+            {
+                counter++;
+                current = current.Next;
+            }
+
+            return counter;
+        }
+
+
+        //2. Counting the amount of duplicated nodes in a Linked List
 
 
     }
