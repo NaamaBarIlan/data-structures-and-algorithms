78ffb59c220c6497eb7ed528fffb11608d314e72
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-02-16 17:13:56 -0800

    Added CountTreeLeaves to DSAPractice/DepthFirst Added a unit test, passing.

--- Challenges/DSAPractice/DSAPractice/DSAPractice/DepthFirst.cs
+++ Challenges/DSAPractice/DSAPractice/DSAPractice/DepthFirst.cs
@@ -6,12 +6,22 @@ namespace DSAPractice
 {
     public class DepthFirst
     {
-
+        /// <summary>
+        /// Counts the number of nodes in a tree recursively,
+        /// using a depth first pre-order traversal
+        /// </summary>
+        /// <param name="tree">The tree that is being traversed</param>
+        /// <returns>The total count of nodes in the tree</returns>
         public static int CountTreeNodesDepth(Tree<int> tree)
         {
             return CountTreeNodesDepth(tree.Root);
         }
 
+        /// <summary>
+        /// Helper method for CountTreeNodesDepth
+        /// </summary>
+        /// <param name="current">The root node of the tree that is being traversed</param>
+        /// <returns>The total count of nodes in the tree</returns>
         private static int CountTreeNodesDepth(Node<int> current)
         {
             if (current == null)
@@ -25,6 +35,36 @@ namespace DSAPractice
             return result;
 
         }
- 
+
+        /// <summary>
+        /// Counts the number of leaves in a tree recursively,
+        /// using a depth first pre-order traversal
+        /// </summary>
+        /// <param name="root">The root node of the tree that is being traversed</param>
+        /// <returns>The total count of leaves in the tree</returns>
+        public static int CountTreeLeaves(Node<int> root)
+        {
+            return CountTreeLeavesHelper(root);
+        }
+
+        private static int CountTreeLeavesHelper(Node<int> current)
+        {
+            if (current == null)
+            {
+                return 0;
+            }
+
+            if (current.LeftChild == null && current.RightChild == null)
+            {
+                return 1;
+            }
+
+            int result = CountTreeLeavesHelper(current.LeftChild) +
+                             CountTreeLeavesHelper(current.RightChild);
+
+            return result;
+        }
+
+
     }
 }
