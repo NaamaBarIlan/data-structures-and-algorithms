9c3b7807de831852501d1b845c0ae31ccc9ad254
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-02-16 17:37:09 -0800

    Added CountTreeLeavesBreadth to DSAPractice > BreadthFirst Added unit test, passing

--- Challenges/DSAPractice/DSAPractice/DSAPractice/BreadthFirst.cs
+++ Challenges/DSAPractice/DSAPractice/DSAPractice/BreadthFirst.cs
@@ -6,6 +6,12 @@ namespace DSAPractice
 {
     public class BreadthFirst
     {
+        /// <summary>
+        /// Counts the number of nodes in a tree iteratively,
+        /// using a breadth first traversal
+        /// </summary>
+        /// <param name="tree">The tree that is being traversed</param>
+        /// <returns>The total count of nodes in the tree</returns>
         public static int CountTreeNodesBreath(Tree<string> tree)
         {
             int counter = 0;
@@ -26,6 +32,43 @@ namespace DSAPractice
                 }
 
                 if (front.RightChild != null)
+                {
+                    que.Enqueue(front.RightChild);
+                }
+            }
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Counts the number of leaves in a tree iteratively,
+        /// using a breadth first traversal
+        /// </summary>
+        /// <param name="root">The root node of the tree that is being traversed</param>
+        /// <returns>The total count of leaves in the tree</returns>
+        public static int CountTreeLeavesBreadth(Node<string> root)
+        {
+            int counter = 0;
+            
+            Queue<Node<string>> que = new Queue<Node<string>>();
+
+            que.Enqueue(root);
+
+            while(que.Count != 0)
+            {
+                Node<string> front = que.Dequeue();
+
+                if(front.LeftChild == null && front.RightChild == null)
+                {
+                    counter++;
+                }
+
+                if(front.LeftChild != null)
+                {
+                    que.Enqueue(front.LeftChild);
+                }
+
+                if(front.RightChild != null)
                 {
                     que.Enqueue(front.RightChild);
                 }
