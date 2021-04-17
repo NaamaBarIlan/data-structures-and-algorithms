dad52395cea21140dbef486852549bcabab18aa0
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-04-17 16:55:48 -0700

    Added notes to BinarySearch

--- Challenges/GrokkingAlgo/01BinarySearch/01BinarySearch/Program.cs
+++ Challenges/GrokkingAlgo/01BinarySearch/01BinarySearch/Program.cs
@@ -4,6 +4,11 @@ namespace _01BinarySearch
 {
     class Program
     {
+        /// <summary>
+        /// This excercise from Grokking Algorithms was converted to C# from Python
+        /// Reference: https://www.manning.com/books/grokking-algorithms
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("01 Binary Search");
@@ -15,29 +20,38 @@ namespace _01BinarySearch
             Console.WriteLine(BinarySearch(myList, 4));
         }
 
-        public static int? BinarySearch(int[] arr, int item)
+        public static int BinarySearch(int[] arr, int item)
         {
+            //Set Low and high to keep track of which part of the list we'll search in.
             int low = 0;
             int high = arr.Length - 1;
 
-            while(low <= high)
+            //While we haven't narrowed it down to one element....
+            while (low <= high)
             {
+                //... check the middle element
                 int mid = (low + high) / 2;
                 int guess = arr[mid];
 
-                if(guess == item)
+                // If the item is found:
+                if (guess == item)
                 {
                     return mid;
 
-                } else if (guess > item)
+                    
+                } // If the guess was too high:
+                else if (guess > item)
                 {
                     high = mid - 1;
 
-                } else
+                } // If the guess was too low:
+                else
                 {
                     low = mid + 1;
                 }
             }
+
+            // If the item doesn't exist
             return -1;
         }
     }
