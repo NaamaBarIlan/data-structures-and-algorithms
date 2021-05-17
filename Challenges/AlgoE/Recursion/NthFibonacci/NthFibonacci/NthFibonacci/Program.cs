2d3fd3944daf629237bbfa4789a2f3606f695875
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-05-16 19:42:08 -0700

    Added 3 solutions for Nth Fibonacci

--- Challenges/AlgoE/Recursion/NthFibonacci/NthFibonacci/NthFibonacci/Program.cs
+++ Challenges/AlgoE/Recursion/NthFibonacci/NthFibonacci/NthFibonacci/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NthFibonacci
 {
@@ -6,7 +7,83 @@ namespace NthFibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Console.WriteLine("NthFibonacci - 3 Solutions");
+
+			// The nth Fibonacci number is: 
+			// fib(n) = fib(n-1) + fib(n-2)  for n > 2
+		}
+
+		// Simple Recursive Solution - worst time
+		// O(2^n) time | O(n) space
+		public static int GetNthFib1(int n)
+		{
+			// base case:
+			if (n == 2)
+			{
+				return 1;
+			}
+
+			if (n == 1)
+			{
+				return 0;
+			}
+
+			// recursive case: 			
+			return GetNthFib1(n - 1) + GetNthFib1(n - 2);
+		}
+
+
+		// Recursive solution with a Dictionary - better time
+		// O(n) time | O(n) space
+
+		public static int GetNthFib2(int n)
+		{
+			Dictionary<int, int> table = new Dictionary<int, int>();
+			table.Add(1, 0);
+			table.Add(2, 1);
+			return GetNthFib2(n, table);
+		}
+
+		private static int GetNthFib2(int n, Dictionary<int, int> table)
+		{
+			// base case:
+			if (table.ContainsKey(n))
+			{
+				return table[n];
+			}
+			// recursive case:
+			else
+			{
+				table.Add(n, GetNthFib2(n - 1, table) + GetNthFib2(n - 2, table));
+				return table[n];
+			}
+
+		}
+
+		// Iterative solution - best space
+		// O(n) time | O(1) space
+		public static int GetNthFib3(int n)
+		{
+
+			int[] lastTwo = { 0, 1 };
+			int counter = 3;
+
+			while (counter <= n)
+			{
+				int nextFib = lastTwo[0] + lastTwo[1];
+				lastTwo[0] = lastTwo[1];
+				lastTwo[1] = nextFib;
+				counter++;
+			}
+
+			if (n > 1)
+			{
+				return lastTwo[1];
+			}
+			else
+			{
+				return lastTwo[0];
+			}
+		}
+	}
 }
