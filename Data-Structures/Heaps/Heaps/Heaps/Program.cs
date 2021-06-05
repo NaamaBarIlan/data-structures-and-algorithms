70f3b0ff05e5acee43cce059ee11f9ed34d0b96e
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-06-04 19:15:13 -0700

    Added Heaps project and readme to DS

---
+++ Data-Structures/Heaps/Heaps/Heaps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Heaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
